ce13dce3e5aaf0960fc9e1a9f7add513a4f75e4f
1lugesya <sariaysegulll@gmail.com>
2023-05-11 22:35:33 +0300

    Initial Commit

---
+++ itunesAPI/itunesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunesAPIApp.swift
+//  itunesAPI
+//
+//  Created by Ayşegül Sarı on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct itunesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
